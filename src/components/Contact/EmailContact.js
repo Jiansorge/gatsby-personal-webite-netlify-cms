273d43c2451555e3227d973d24e11499216d95ca
[jiansorge] <[ji@nsorge.com]>
2020-06-13 16:28:07 -0700

    updated email contact

--- src/components/Contact/EmailContact.js
+++ src/components/Contact/EmailContact.js
@@ -8,23 +8,23 @@ class EmailContact extends React.Component {
     super(props)
     this.state = {
       status: "",
-      inputValue:"",
+      inputValue: "",
     }
   }
 
-  updateInputValue = (ev) => {
-    this.setState( {
-      inputValue: ev.target.value
-    })
-  }
-
-  clear =() => {
+  updateInputValue = ev => {
     this.setState({
-      inputValue: "asdlfkjasdlkj"
+      inputValue: ev.target.value,
     })
   }
 
-  submitForm= (ev)=> {
+  clear = () => {
+    this.setState({
+      inputValue: "asdlfkjasdlkj",
+    })
+  }
+
+  submitForm = ev => {
     ev.preventDefault()
     const form = ev.target
     const data = new FormData(form)
@@ -43,131 +43,142 @@ class EmailContact extends React.Component {
     xhr.send(data)
   }
 
-  render(){
+  render() {
     const { status } = this.state
     return (
       <div>
-      <section>
-        <Title title="Contact " subtitle="   Form" />
-        <div>
-          <form
-            onSubmit={this.submitForm}
-            action="https://formspree.io/meqrkgzr"
-            method="POST"
-            className={styles.form}
-          >
-            <div>
-              <label htmlFor="name">name</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className={styles.formControl}
-                placeholder={emailStrings.namePlaceholder}
-                aria-label="what's is your name"
+        <section>
+          <Title title="Contact " subtitle="   Form" />
+          <div>
+            <form
+              onSubmit={this.submitForm}
+              action="https://formspree.io/meqrkgzr"
+              method="POST"
+              className={styles.form}
+            >
+              <div>
+                <label htmlFor="name">name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className={styles.formControl}
+                  placeholder={emailStrings.namePlaceholder}
+                  aria-label="what's is your name"
+                />
+              </div>
 
-              />
-            </div>
+              <div>
+                <label htmlFor="email">email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className={styles.formControl}
+                  placeholder={emailStrings.emailPlaceholder}
+                  aria-label="what is your email"
+                />
+              </div>
+              <div>
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  rows="10"
+                  className={styles.formControl}
+                  placeholder={emailStrings.messagePlaceholder}
+                  aria-label="write your message"
+                />
+              </div>
+              <div>
+                {status === "SUCCESS" ? (
+                  <p>Thanks!</p>
+                ) : (
+                  this.state.inputValue.length < 1 && (
+                    <button
+                      value={emailStrings.submitText}
+                      className={styles.submit}
+                      onSubmit={this.submitForm}
+                      aria-label="Send Message"
+                    >
+                      Submit
+                    </button>
+                  )
+                )}
+              </div>
 
+              {status === "ERROR" && <p>Ooops! There was an error.</p>}
+            </form>
+          </div>
+        </section>
 
-            <div>
-              <label htmlFor="email">email</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className={styles.formControl}
-                placeholder={emailStrings.emailPlaceholder}
-                aria-label="what is your email"
-
-              />
-            </div>
-            <div>
-              <label htmlFor="message">Message</label>
-              <textarea
-                name="message"
-                id="message"
-                rows="10"
-                className={styles.formControl}
-                placeholder={emailStrings.messagePlaceholder}
-                aria-label="write your message"
-              />
-            </div>
-            <div>
-              {status === "SUCCESS"  
-              ? (
-                <p>Thanks!</p>
-              ) : (
-                this.state.inputValue.length < 1
-                && <button
-                  value={emailStrings.submitText}
-                  className={styles.submit}
-                  onSubmit={this.submitForm}
-                  aria-label="Send Message"
-                >Submit</button>
-              )}
-            </div>
-
-            {status === "ERROR" && <p >Ooops! There was an error.</p>}
+        <aside>
+          <form>
+            <label htmlFor="f_name" aria-label="not real input"></label>
+            <input
+              name="f_name"
+              type="hidden"
+              value=""
+              aria-label="not real input"
+              onChange={this.updateInputValue}
+            />
+            <label
+              htmlFor="g_name"
+              aria-label="not real input"
+              style={{ position: "absolute", top: "3px", left: "-680px" }}
+            ></label>
+            <input
+              name="g_name"
+              type="text"
+              value=""
+              style={{ position: "absolute", top: "3px", left: "-680px" }}
+              onChange={this.updateInputValue}
+              aria-label="not real input"
+            />
+            <label
+              htmlFor="j_name"
+              aria-label="not real input"
+              style={{ display: "none" }}
+            ></label>
+            <input
+              name="j_name"
+              type="text"
+              value=""
+              style={{ display: "none" }}
+              onChange={this.updateInputValue}
+              aria-label="not real input"
+            />
+            <label
+              htmlFor="k_name"
+              aria-label="not real input"
+              style={{ visibility: "hidden" }}
+            ></label>
+            <input
+              name="k_name"
+              type="text"
+              value=""
+              style={{ visibility: "hidden" }}
+              onChange={this.updateInputValue}
+              aria-label="not real input"
+            />
+            <label
+              htmlFor="l_name"
+              aria-label="not real input"
+              style={{ visibility: "collapse" }}
+            ></label>
+            <input
+              name="l_name"
+              type="text"
+              value=""
+              style={{ visibility: "collapse" }}
+              onChange={this.updateInputValue}
+              aria-label="not real input"
+            />
           </form>
-        </div>
-      </section>
-
-
-
-      <aside>
-        <form>
-          <label htmlFor="f_name" aria-label="not real input"></label>
-          <input name="f_name" type="hidden" value=""                 aria-label="not real input"
-          onChange={this.updateInputValue}
-          />
-          <label htmlFor="g_name" aria-label="not real input"                     style={{ position: "absolute", top: "3px", left: "-680px" }}
-          ></label>
-          <input
-            name="g_name"
-            type="text"
-            value=""
-            style={{ position: "absolute", top: "3px", left: "-680px" }}
-            onChange={this.updateInputValue}
-            aria-label="not real input"
-          />
-          <label htmlFor="j_name" aria-label="not real input"                     style={{'display':'none'}}
-          ></label>
-          <input
-            name="j_name"
-            type="text"
-            value=""
-            style={{'display':'none'}}
-            onChange={this.updateInputValue}
-            aria-label="not real input"
-          />
-          <label htmlFor="k_name" aria-label="not real input" style={{'visibility':'hidden'}}></label>
-          <input
-            name="k_name"
-            type="text"
-            value=""
-            style={{'visibility':'hidden'}}
-            onChange={this.updateInputValue}
-            aria-label="not real input"
-          />
-          <label htmlFor="l_name" aria-label="not real input"               style={{'visibility':'collapse'}}
-          ></label>
-          <input
-            name="l_name"
-            type="text"
-            value=""
-            style={{'visibility':'collapse'}}
-            onChange={this.updateInputValue}
-            aria-label="not real input"
-          />
-
-        </form>
-      </aside>
+        </aside>
       </div>
     )
   }
-  
-
 }
 
 export default EmailContact
